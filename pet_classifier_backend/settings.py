70b92f0b6479539509be06f558d92c97272f8792
Sergio Pinzon <sergiopin596@gmail.com>
2025-07-27 17:54:46 -0500

    feat: add CORS_ALLOWED_ORIGINS for local development

--- pet_classifier_backend/settings.py
+++ pet_classifier_backend/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = ['https://petclassifier.zeabur.app/',
                  'http://127.0.0.1:3000',
                  ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 # Application definition
 
